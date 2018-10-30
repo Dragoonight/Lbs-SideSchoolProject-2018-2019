3b75e3489ebd0e871d266aab141efd9cac64fc0e
Alexander Öberg <stativetroller7@gmail.com>
2018-10-30 18:37:43 +0100

    Added enemymanager.cs

---
+++ Lbs.groupproject.2018-2019/Lbs.groupproject.2018-2019/Lbs.groupproject._2018_2019/EnemyManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Lbs.groupproject._2018_2019
+{
+    internal static class EnemyManager
+    {
+        /// <summary>
+        /// List of all alive Enemies
+        /// </summary>
+        public static List<Enemy> Enemies = new List<Enemy>();
+
+        public static void AddEnemy(Enemy enemy)
+        {
+            Enemies.Add(enemy);
+        }
+
+        public static void ClearEnemies()
+        {
+            Enemies.Clear();
+        }
+
+        public static bool CheckCollisionToPlayer()
+        {
+            foreach (Enemy enemy in Enemies)
+            {
+                if (enemy.CollidableObject.IsColliding(InGame.Player.CollidableObject))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static void Update(GameTime gameTime)
+        {
+            foreach (Enemy enemy in Enemies)
+            {
+                enemy.Update(gameTime);
+            }
+        }
+
+        public static void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (Enemy enemy in Enemies)
+            {
+                enemy.Draw(spriteBatch);
+            }
+        }
+    }
+}
